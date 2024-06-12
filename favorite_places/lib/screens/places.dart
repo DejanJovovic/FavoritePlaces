55a43f2e7302748750dec2c76c3293fdadfbc326
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2024-06-12 18:11:35 +0200

    added add_place screen

--- favorite_places/lib/screens/places.dart
+++ favorite_places/lib/screens/places.dart
@@ -1,10 +1,9 @@
+import 'package:favorite_places/screens/add_place.dart';
 import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 
 class PlacesScreen extends StatelessWidget {
-
   const PlacesScreen({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +12,20 @@ class PlacesScreen extends StatelessWidget {
         title: const Text('Your places'),
         actions: [
           IconButton(
-            onPressed: (){}, 
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
             icon: const Icon(Icons.add),
-            ),
+          ),
         ],
       ),
       body: PlacesList(
         places: [],
-        ),
+      ),
     );
   }
 }
