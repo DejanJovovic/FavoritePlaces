bb553bf2230969add1c328479bae0105897b9b9d
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2024-06-13 14:24:57 +0200

    with the location and geocoding packages user is allowed to get the current location on the map, and save it on screen with the image and title

--- favorite_places/lib/models/place.dart
+++ favorite_places/lib/models/place.dart
@@ -4,14 +4,29 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+class PlaceLocation {
+
+  const PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
+
+  final double latitude;
+  final double longitude;
+  final String address;
+}
+
 class Place {
 
   Place({
     required this.image,
-    required this.title}) : id = uuid.v4();
+    required this.title,
+    required this.location}) : id = uuid.v4();
 
   final String id;
   final String title;
   final File image;
+  final PlaceLocation location;
 
 }
--- favorite_places/lib/screens/add_place.dart
+++ favorite_places/lib/screens/add_place.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+import 'package:favorite_places/models/place.dart';
 import 'package:favorite_places/providers/user_places.dart';
 import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geocoding/geocoding.dart' as geo;
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -17,15 +20,40 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
   File? _selectedImage;
+  PlaceLocation? _selectedLocation;
 
-  void _savePlace(){
+  Future<List> getLocationAddress(double latitude, double longitude) async {
+    List<geo.Placemark> placemark =
+        await geo.placemarkFromCoordinates(latitude, longitude);
+    return placemark;
+  }
+
+  void _selectPlace(double latitude, double longitude) async {
+    final addressData = await getLocationAddress(latitude, longitude);
+
+    final String street = addressData[0].street;
+    final String postalcode = addressData[0].postalCode;
+    final String locality = addressData[0].locality;
+    final String country = addressData[0].country;
+    final String address = '$street, $postalcode, $locality, $country';
+
+    _selectedLocation = PlaceLocation(
+        latitude: latitude, longitude: longitude, address: address);
+  }
+
+  void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if(enteredTitle.isEmpty || _selectedImage == null) {
+    if (enteredTitle.isEmpty ||
+        _selectedImage == null ||
+        _selectedLocation == null) {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle, _selectedImage!);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredTitle, _selectedImage!, _selectedLocation!);
+
     Navigator.of(context).pop();
   }
 
@@ -39,7 +67,7 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add new place'),
+        title: const Text('Add new Place'),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(12),
@@ -52,21 +80,19 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onSurface,
               ),
             ),
-            const SizedBox(
-              height: 10,
-            ),
+            const SizedBox(height: 10),
             ImageInput(
-              onPickImage: (image){
+              onPickImage: (image) {
                 _selectedImage = image;
               },
-            ), 
-            const SizedBox(
-              height: 16,
             ),
+            const SizedBox(height: 10),
+            LocationInput(onSelectPlace: _selectPlace),
+            const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
               icon: const Icon(Icons.add),
-              label: const Text('Add place'),
+              label: const Text('Add Place'),
             ),
           ],
         ),
